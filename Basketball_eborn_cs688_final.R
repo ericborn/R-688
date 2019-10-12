cf6d8a4b3a27f999726e2af4c2988024297acb91
Eric Born <eric.born85@gmail.com>
2019-10-11 20:26:07 -0700

    Add files via upload

--- Basketball_eborn_cs688_final.R
+++ Basketball_eborn_cs688_final.R
@@ -68,24 +68,24 @@ for (k in 2:8){
 # atl div 7 teams
 # loop through the columns grabbing each win and loss
 for (k in 2:8){
-  atl.wins[[k]] <- webpage %>% html_nodes("table") %>% .[4] %>% 
+  atl.wins[[k]] <- webpage %>% html_nodes("table") %>% .[6] %>% 
     html_nodes("tr") %>% .[k] %>% html_nodes("td") %>% .[3] %>% html_text()
 }
 
 for (k in 2:8){
-  atl.loss[[k]] <- webpage %>% html_nodes("table") %>% .[4] %>% 
+  atl.loss[[k]] <- webpage %>% html_nodes("table") %>% .[6] %>% 
     html_nodes("tr") %>% .[k] %>% html_nodes("td") %>% .[4] %>% html_text()
 }
 
 # cen div 8 teams
 # loop through the columns grabbing each win and loss
 for (k in 2:9){
-  cen.wins[[k]] <- webpage %>% html_nodes("table") %>% .[5] %>% 
+  cen.wins[[k]] <- webpage %>% html_nodes("table") %>% .[7] %>% 
     html_nodes("tr") %>% .[k] %>% html_nodes("td") %>% .[3] %>% html_text()
 }
 
 for (k in 2:9){
-  cen.loss[[k]] <- webpage %>% html_nodes("table") %>% .[5] %>% 
+  cen.loss[[k]] <- webpage %>% html_nodes("table") %>% .[7] %>% 
     html_nodes("tr") %>% .[k] %>% html_nodes("td") %>% .[4] %>% html_text()
 }
 
@@ -99,47 +99,29 @@ atl.loss <- atl.loss[-1]
 cen.wins <- cen.wins[-1]
 cen.loss <- cen.loss[-1]
 
-# team names by division
+# team names by conference and division
 western <- webpage %>% html_nodes("table") %>% .[1] %>% html_nodes("a") %>% html_text()
 eastern <- webpage %>% html_nodes("table") %>% .[2] %>% html_nodes("a") %>% html_text()
 midwest <- webpage %>% html_nodes("table") %>% .[4] %>% html_nodes("a") %>% html_text()
 pacific <- webpage %>% html_nodes("table") %>% .[5] %>% html_nodes("a") %>% html_text()
 atlantic <- webpage %>% html_nodes("table") %>% .[6] %>% html_nodes("a") %>% html_text()
 central <- webpage %>% html_nodes("table") %>% .[7] %>% html_nodes("a") %>% html_text()
-teams <- c(Midwest, Pacific, Atlantic, Central)
+teams <- c(midwest, pacific, atlantic, central)
+
 
 teams.df = data.frame(Team=teams, Conference=c(rep('Western',length(western)),
                                               rep('Eastern',length(eastern))),
                       Division=c(rep('Midwest', length(midwest)),
                                  rep('Pacific', length(pacific)),
                                  rep('Atlantic', length(atlantic)),
-                                 rep('Central', length(central))
-                                 ),Win=c(west.wins, ),Loss='')
+                                 rep('Central', length(central))),
+                                 Win=c(mid.wins, pac.wins, atl.wins, cen.wins),
+                                 Loss=c(mid.loss,pac.loss,atl.loss,cen.loss))
 
-teams.df$Division = 1
+# output top 5 teams by total wins
+head(teams.df[order(teams.df$Win, decreasing = TRUE),], n=5)
 
-cbind(teams.df,Team=teams)
-
-teams.df[teams.df$Team %in% western,] = 1
-
-
-# b)
-# subset of only players who made greater than 300 shots
-fg <- NBA.Stats[NBA.Stats$FieldGoalsMade > 299,]
-
-
-# create new column of shot percentage
-fg$pct <- fg[,7]/fg[,8]
-
-# find the row equal to the max pct row
-# highest field goal percentage
-fg[fg$pct == max(fg$pct),c(2,7,8,26)]
-
-# c)
-# top 10 players in terms of total points
-head(NBA.Stats[order(NBA.Stats$TotalPoints, decreasing = TRUE),c(2,3,21)], n=10)
-
-# d)
+# e)
 # 1)
 # plot top 10 points per team
 # gather total points per team
@@ -160,41 +142,32 @@ all.points <- aggregate(TotalPoints ~ Team, data = NBA.Stats, FUN=sum)[-19,]
 # average points across all teams
 avg.points <- mean(all.points$TotalPoints)
 
-# creates avg as a new column
-# I dont really like the way it looks vs an avg line
-#avg.points <- data.frame('Avg', mean(all.points$TotalPoints))
-#names(avg.points) <- c('Team', 'TotalPoints')
-#top10.points <- rbind(top10.points, avg.points)
-
 y <- list(title = "Total Points")
 x <- list(title = 'Teams')
-points.plot <- plot_ly(top10.points, x = ~Team, y=~TotalPoints, name = 'Total Points', type='bar')%>% 
-  add_trace(y =avg.points, name = 'League Avg Points', type = 'scatter', mode = 'lines')%>%
+points.plot <- plot_ly(top10.points, x = ~Team, y= ~TotalPoints,
+                       type='bar', color = ~Team)%>% 
+  add_trace(y = avg.points, name = 'League Avg Points', type = 'scatter', 
+            mode = 'lines', color = I('black'))%>%
   layout(yaxis = y, title = "Top 10 Total Points per Team", xaxis = x)
 
 # draw plot
 points.plot
 
-# 2)
-# top 10 total minutes played
-top.minutes <- aggregate(TotalMinutesPlayed ~ Team, data = NBA.Stats, FUN=sum)
+# # 2)
+# scorers breakdown for the wolves
+# number of FieldGoalsMade, ThreesMade, FreeThrowsMade
+wolves.points <- wolves[c(2,7,9,11)]
 
-# only select top 10 teams by total points
-top10.minutes <- head(top.minutes[order(top.minutes$TotalMinutesPlayed, decreasing = TRUE),], n=10)
-
-# Reset rownames from 1 to n
-rownames(top10.minutes) <- 1:nrow(top10.minutes)
-
-# drop empty factor levels
-top10.minutes <- droplevels(top10.minutes)
-
-y <- list(title = "Total Minutes")
-x <- list(title = 'Teams')
-minutes.plot <- plot_ly(top10.minutes, x = ~Team, y=~TotalMinutesPlayed, type='bar')%>% 
-  layout(yaxis = y, title = "Total Minutes per Team", xaxis = x)
+# Create boxplot based on top 10 beer styles with ABV information
+y <- list(title = "Total Points")
+x <- list(title = 'Player')
+wolves.plot <- plot_ly(wolves.points, x = ~Name, y = ~FieldGoalsMade, type = 'bar', name = 'Field Goals')%>% 
+  add_trace(y = ~ThreesMade, name =  'Threes' )%>%
+  add_trace(y = ~FreeThrowsMade, name = 'Free Throws' )%>%
+  layout(xaxis = x, yaxis = y, title = "Point Distribution of the Timber Wolves", barmode = 'stack')
 
 # draw plot
-minutes.plot
+wolves.plot
 
 # 3)
 # Box plot
@@ -213,7 +186,8 @@ top10.full <- droplevels(top10.full)
 # Create boxplot based on top 10 beer styles with ABV information
 y <- list(title = "Total Points")
 x <- list(title = 'Team')
-top.box <- plot_ly(top10.full, x = ~Team, y = ~TotalPoints, type = 'box', size = 2)%>% 
+top.box <- plot_ly(top10.full, x = ~Team, y = ~TotalPoints, type = 'box', size = 2,
+                   color = ~Team)%>% 
   layout(xaxis = x, yaxis = y, title = "Point distribution of top 10 teams")
 
 # draw plot
@@ -264,3 +238,27 @@ team.point.plot <- plot_ly(team.points, x = ~Team, y = ~FieldGoalsMade, type = '
 
 # draw plot
 team.point.plot
+
+# f)
+# champ names
+
+champ.url <- 'https://www.landofbasketball.com/championships/year_by_year.htm'
+
+# read the html
+champ.page <- read_html(champ.url)
+
+# initalize vector
+champ.names <- c(0)
+
+# loop through page to get the last 20 NBA champs
+for (k in 2:21){
+  champ.names[[k]] <- champ.page %>% html_nodes("table") %>% .[1] %>% html_nodes("tr") %>% .[k] %>% 
+    html_nodes("a") %>% .[2] %>% html_text()
+}
+
+# drop index 1
+champ.names <- champ.names[-1]
+
+# strsplit(champ.names, " ")
+
+city.url 'https://en.wikipedia.org/wiki/National_Basketball_Association'``
